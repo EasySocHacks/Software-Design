b015cfcc58e5971b0580f0d920433b760691b205
EasySocHacks <easy.soc.hacks@gmail.com>
2021-09-25 19:34:37 +0300

    Remove peronal info

--- HW2/src/main/java/Main.java
+++ HW2/src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
      * @param args System arguments.
      */
     public static void main(String[] args) {
-        API vkAPI = new VkAPI("6c5954876c5954876c595487946c20275e66c596c5954870d14c49e1eb4e742a45b862f");
+        API vkAPI = new VkAPI("VK TOKEN");
 
         PostStatistic postStatistic = new PostStatistic(vkAPI);
         System.out.println(postStatistic.getPostCountByTagForPeriod("#утро", 3));
