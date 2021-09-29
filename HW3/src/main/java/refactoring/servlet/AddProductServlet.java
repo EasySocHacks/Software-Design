770fc94411ea8cc859d26a28f8f9ecde2a7d5cc6
EasySocHacks <easy.soc.hacks@gmail.com>
2021-09-29 20:34:48 +0300

    Change AddProductServlet realization (not logic) using AbstractServlet

--- HW3/src/main/java/refactoring/servlet/AddProductServlet.java
+++ HW3/src/main/java/refactoring/servlet/AddProductServlet.java
@@ -1,48 +1,27 @@
 package refactoring.servlet;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 
 /**
  * @author akirakozov
  */
-public class AddProductServlet extends HttpServlet {
-    private String databaseUrl = "jdbc:sqlite:test.db";
-    private String user = "";
-    private String password = "";
-
-    public AddProductServlet() { }
+public class AddProductServlet extends AbstractServlet {
+    public AddProductServlet() {
+        super();
+    }
 
     public AddProductServlet(String databaseUrl, String user, String password) {
-        this.databaseUrl = databaseUrl;
-        this.user = user;
-        this.password = password;
+        super(databaseUrl, user, password);
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    void doGetMainLogic(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String name = request.getParameter("name");
         long price = Long.parseLong(request.getParameter("price"));
 
-        try {
-            try (Connection c = DriverManager.getConnection(databaseUrl, user, password)) {
-                String sql = "INSERT INTO PRODUCT " +
-                        "(NAME, PRICE) VALUES ('" + name + "'," + price + ")";
-                Statement stmt = c.createStatement();
-                stmt.executeUpdate(sql);
-                stmt.close();
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("OK");
+        databaseUtils.getStatement().executeUpdate(
+                "INSERT INTO PRODUCT (NAME, PRICE) VALUES " +
+                        "('" + name + "'," + price + ")");
     }
 }
