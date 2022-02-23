c488c67313b58425db60bccff9e28f6101d28263
EasySocHacks <easy.soc.hacks@gmail.com>
2022-02-23 22:21:14 +0300

    Delete useless

--- S8/HW2/HW2/src/main/kotlin/easy/soc/hacks/hw2/repository/ProductRepository.kt
+++
@@ -1,8 +0,0 @@
-package easy.soc.hacks.hw2.repository
-
-import easy.soc.hacks.hw2.domain.Product
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ProductRepository: ReactiveMongoRepository<Product, String>
